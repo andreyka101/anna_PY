ad0edab73160e01ed502eb5364bf5e397ee406b2
andreyka101 <andrejkvasnin236@gmail.com>
2024-09-18 21:58:38 +0300

    efefef

--- lesson_1.py
+++ lesson_1.py
@@ -2,20 +2,19 @@
 a1 = 4.6
 spi=[1,2,3,2]
 print(spi.count(2))
-print("uuu")
 
-spi_2 = spi
-spi_copy = spi.copy()
-spi.append(88)
-print(spi_2)
-print(spi_copy)
+# spi_2 = spi
+# spi_copy = spi.copy()
+# spi.append(88)
+# print(spi_2)
+# print(spi_copy)
 
 
 # for i in spi:
 #     print(i*2)
 
-for i in range(len(spi)):
-    print(spi[i])
+# for i in range(len(spi)):
+#     print(spi[i])
 
 # i = 0
 # while i != len(spi):
---
+++ lesson_2_metods_list.py
@@ -0,0 +1,62 @@
+arr = [1,2,3,4,5,1]
+print(arr)
+
+
+# list.append(x) - Добавляет элемент в конец списка
+# arr.append(99)
+# print(arr)
+
+
+# list.extend(L) - Расширяет список list, добавляя в конец все элементы списка L
+# arr.append([9,8,7])
+# arr.extend([9,8,7])
+# print(arr)
+
+
+# list.insert(i, x) - Вставляет на i-ый элемент значение x
+# arr.insert(1,77)
+# print(arr)
+
+
+# list.remove(x) - Удаляет первый элемент в списке, имеющий значение x. ValueError, если такого элемента не существует
+# arr.remove(2)
+# print(arr)
+
+
+# list.pop(i) - Удаляет i-ый элемент и возвращает его. Если индекс не указан, удаляется последний элемент
+# arr.pop(2)
+# print(arr)
+
+
+# list.index(x, [start [, end]]) - Возвращает положение первого элемента со значением x (при этом поиск ведется от start до end)
+# print(arr.index(3))
+
+
+# list.count(x) - Возвращает количество элементов со значением x
+# print(arr.count(1))
+
+
+# list.sort([key=функция]) - Сортирует список на основе функции
+# arr.sort()
+# print(arr)
+
+
+# list.reverse() - Разворачивает список
+# arr.reverse()
+# print(arr)
+
+
+# list.copy() - Поверхностная копия списка
+# arr_2 = arr
+# copy_arr = arr.copy()
+# print(arr_2)
+# arr.append(999)
+# print(arr)
+# print(arr_2)
+# print(copy_arr)
+
+
+# list.clear() - Очищает список
+# arr.clear()
+# print(arr)
+
